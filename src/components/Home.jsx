56b41f1053ef393dd14fbaae9ed65b6e3ff6dfcb
Bereket Mengesha <101510735+ephrem20@users.noreply.github.com>
2025-06-07 08:43:45 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
             className='bg-gradient-to-r from-green-400 to-blue-500 text-white
             transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'
             onClick={() => window.location.href = '#contact'}>
-            Contact With Me</button>
+            Contact Me</button>
             <button
             className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white
             transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'
