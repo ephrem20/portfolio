b687bee1e0e4075fe29fc5677cf105b1edb94ad1
Bereket Mengesha <101510735+ephrem20@users.noreply.github.com>
2025-12-03 13:43:35 +0300

    Add link to BereketMengesha.pdf in Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import homepic from '../assets/homepic.jpg'
+import BereketMengesha '../assets/BereketMengesha.pdf'
 
 const Home = () => {
   return (
@@ -27,7 +28,7 @@ const Home = () => {
             <button
             className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white
             transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'
-            onClick={() =>{window.open('/resume.png', '_blank','w-800,h-800');
+            onClick={() =>{window.open('/BereketMengesha.pdf', '_blank','w-800,h-800');
             }}
             >Resume</button>
         </div>
